eb1da2bf3aa9803fc9b31f3122da35a782669a18
tan nguyen <tannpv@gmail.com>
2026-03-28 20:11:24 +0700

    feat: add clipboard helper with paste simulation

---
+++ DraftRight/Utils/ClipboardHelper.swift
@@ -0,0 +1,20 @@
+import AppKit
+import Carbon.HIToolbox
+
+enum ClipboardHelper {
+    static func copy(text: String) {
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(text, forType: .string)
+    }
+
+    static func pasteFromClipboard() {
+        let source = CGEventSource(stateID: .hidSystemState)
+        let keyDown = CGEvent(keyboardEventSource: source, virtualKey: CGKeyCode(kVK_ANSI_V), keyDown: true)
+        keyDown?.flags = .maskCommand
+        let keyUp = CGEvent(keyboardEventSource: source, virtualKey: CGKeyCode(kVK_ANSI_V), keyDown: false)
+        keyUp?.flags = .maskCommand
+        keyDown?.post(tap: .cgSessionEventTap)
+        keyUp?.post(tap: .cgSessionEventTap)
+    }
+}
